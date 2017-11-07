f363a72c1d8fef87b6e5d21216f351c9e4b2a18b
Bogdanov Ilya <onesuch07@gmail.com>
2017-11-07 15:01:55 +0300

    Добавил кеширование файлов, картинок. Подправил структуру js

--- src/app/section/section.component.ts
+++ src/app/section/section.component.ts
@@ -1,12 +1,13 @@
 import {Component} from '@angular/core';
 
-@Component({ // @Component - знак декоратор
-    selector: 'app-section', //  Это корневой компонент, находится в index.html ( название должно иметь app - апостроф обязательно " app- ")
+@Component({
+    selector: 'app-section',
     templateUrl: './section.component.html',
     styleUrls: ['./section.component.less']
 })
+
 export class SectionComponent {
-    title: 'Title';
-    body: 'Тело нашего компонента';
-    bottomText: 'Нижний текст с небольшим описанием';
+    title = 'Title';
+    body = 'Тело нашего компонента';
+    text = 'Нижний текст с небольшим описанием';
 }
